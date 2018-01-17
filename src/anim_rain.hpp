f1c5bd959e2792979a7026fc10ed086db411a7ca
Matthias Treydte <mt@waldheinz.de>
2018-01-17 21:27:03 +0100

    first try at a rain animation

---
+++ src/anim_rain.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "anim.hpp"
+
+class anim_rain final : public animation {
+public:
+    anim_rain() : anims(COLUMNS) {
+        for (int i = 0; i < COLUMNS; i++) {
+            start(i);
+        }
+    }
+
+    void loop() override {
+        anims.UpdateAnimations();
+    }
+
+private:
+    void start(uint16_t i) {
+        anims.StartAnimation(i, 500 + random(2500),
+                std::bind(&anim_rain::animate, this, std::placeholders::_1));
+    }
+
+    void animate(const AnimationParam& param) {
+        if (param.state == AnimationState_Completed) {
+            start(param.index);
+        } else if (param.state == AnimationState_Progress) {
+            float drop_y = ROWS * (1 - param.progress);
+
+            for (int row = 0; row < ROWS; row++) {
+                const float d = std::min(std::max(fabsf(row - drop_y) / 2.0f, 0.0f), 1.0f);
+                const float de = NeoEase::CubicInOut(d);
+                const RgbwColor c = colorGamma.Correct(RgbwColor::LinearBlend(RgbwColor(0, 15, 64), RgbwColor(0), de));
+
+                strip.SetPixelColor(to_index(param.index, row), c);
+            }
+        }
+    }
+
+    NeoPixelAnimator anims;
+};
