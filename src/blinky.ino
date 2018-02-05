ed0d39adf03b844d8d74a61c2451d65ae0e88a64
Matthias Treydte <mt@waldheinz.de>
2018-02-05 18:31:33 +0100

    allow to switch animations by button press

--- src/blinky.ino
+++ src/blinky.ino
@@ -1,6 +1,8 @@
 #include <NeoPixelBus.h>
 #include <NeoPixelAnimator.h>
 
+#include <memory>
+
 #include "anim.hpp"
 #include "anim_fire.hpp"
 #include "anim_lighthouse.hpp"
@@ -23,25 +25,80 @@ void SetRandomSeed() {
 }
 
 output out;
-animation* anim = nullptr;
+std::unique_ptr<animation> anim = nullptr;
 Encoder knob(D1, D2);
 int32_t knob_last = 0;
+uint32_t anim_idx = 0;
+bool btn_handled = false;
+unsigned long last_btn_input = 0;
+long const debounce_delay = 50; /* ms */
 
 void setup() {
     out.show();
     SetRandomSeed();
     knob.write(knob_last);
+
+    pinMode(D3, INPUT | INPUT_PULLUP);
+    pinMode(D0, OUTPUT);
+    digitalWrite(D0, 1);
+
+    anim = std::unique_ptr<anim_fire>(new anim_fire(&out));
 }
 
 void loop() {
-    if (!anim) {
-        anim = new anim_fire(&out);
-    }
 
+    /* adjust brightness */
     const int32_t knob_now = knob.read();
     if (std::abs(knob_now - knob_last) >= 4) {
         out.adjust_brightness(knob_now < knob_last);
         knob_last = knob_now;
+    }
+
+    /* toggle animations */
+    auto const btn_state = digitalRead(D3);
+
+
+    long const now = millis();
+
+    if ((now - last_btn_input) > debounce_delay) {
+        last_btn_input = now;
+
+        if (!btn_state && !btn_handled) {
+            auto const ANIM_COUNT = 6;
+            anim_idx = (anim_idx + 1) % ANIM_COUNT;
+
+            digitalWrite(D0, anim_idx % 2);
+
+            switch (anim_idx) {
+            case 0:
+                anim = std::unique_ptr<animation>(new anim_fire(&out));
+                break;
+
+            case 1:
+                anim = std::unique_ptr<animation>(new anim_lighthouse(&out));
+                break;
+
+            case 2:
+                anim = std::unique_ptr<animation>(new anim_plasma(&out));
+                break;
+
+            case 3:
+                anim = std::unique_ptr<animation>(new anim_rain(&out));
+                break;
+
+            case 4:
+                anim = std::unique_ptr<animation>(new anim_starry_night(&out));
+                break;
+
+            default:
+                anim = std::unique_ptr<animation>(new anim_warp_core(&out));
+                break;
+            }
+
+            btn_handled = true;
+        } else if (btn_state) {
+            btn_handled = false;
+        }
     }
 
     anim->loop();
